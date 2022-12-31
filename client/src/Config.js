eb1e79d0659df841dc7036efc91c4b25699c49ca
unknown <babawale.emmanuel10@gmail.com>
2022-12-31 16:30:08 +0100

    api updated

--- client/src/Config.js
+++ client/src/Config.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 
 export const axiosInstance = axios.create({
-  baseURL: "https://babz-amazonn.herokuapp.com/api/",
+  baseURL: "https://courageous-garment-colt.cyclic.app/",
 });
