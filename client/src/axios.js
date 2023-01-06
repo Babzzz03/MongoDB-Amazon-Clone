be178b171d2a39eb53de9798be75f210de27679a
Babawale Emmanuel <babawale.emmanuel10@gmail.com>
2023-01-06 13:22:10 +0100

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://courageous-garment-colt.cyclic.app/",
+  baseURL: "https://courageous-garment-colt.cyclic.app/api/",
 });
 
 export default instance;
